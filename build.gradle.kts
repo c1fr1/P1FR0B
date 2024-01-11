5add2284e8442990f60fd3056944f401fd374210
c1fr1 <24968794+c1fr1@users.noreply.github.com>
2024-01-11 14:23:10 -0800

    updated kotlin version and jda version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
 }
 
 plugins {
-	kotlin("jvm") version "1.6.21"
+	kotlin("jvm") version "1.9.22"
 	application
 	id("com.github.johnrengelman.shadow") version "7.1.0"
 }
@@ -28,21 +28,14 @@ dependencies {
 	implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.6.21")
 	implementation("org.reflections", "reflections", "0.9.11")
 	implementation("org.slf4j", "slf4j-nop", "1.7.36")
-	implementation("net.dv8tion", "JDA", "5.0.0-beta.18")
+	implementation("net.dv8tion", "JDA", "5.0.0-beta.19")
 	implementation("com.google.code.gson", "gson", "2.10.1")
 	implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		jvmTarget = "1.8"
-	}
+	kotlinOptions.jvmTarget = "1.8"
 }
-val compileKotlin : KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-	jvmTarget = "1.8"
-}
-val compileTestKotlin : KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-	jvmTarget = "1.8"
+tasks.withType<JavaCompile> {
+	targetCompatibility = "1.8"
 }
